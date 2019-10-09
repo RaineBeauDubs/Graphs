dbb303355c95089aa67579ed9c9bb8051a946443
Raine Wallace <rainebeaudubs@gmail.com>
2019-10-09 18:51:17 -0400

    got islands working and running from guided project, and populate graph working in social, with help.

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,4 +1,17 @@
+import random
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
 class User:
     def __init__(self, name):
@@ -44,11 +57,38 @@ class SocialGraph:
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
-
+        
         # Add users
+        if numUsers <= avgFriendships:
+            print('CAUTION: You cannot have more friendships than users!')
+        else:
+            for i in range(numUsers):
+                self.addUser(f'User {i+1}')
+            # Create friendships
+            potentialFriendships = []
+            for userID in self.users:
+                for friendID in range(userID + 1, self.lastID + 1):
+                    potentialFriendships.append((userID, friendID))
+            random.shuffle(potentialFriendships)
+            for friendship in potentialFriendships[:numUsers]:
+                self.addFriendship(friendship[0], friendship[1])
 
-        # Create friendships
+    def bfs(self, starting_vertex, search_vertex):
+        queue = Queue()
+        visited = set()
+        queue.enqueue([starting_vertex])
+        while queue.size() > 0:
+            path = queue.dequeue()
+            v = path[-1]
+            visited.add(v)
+            if v not in visited:
+                visited.add(v)
+                if v == search_vertex:
+                    return path
+                for neighbor in self.friendships[v]:
+                    new_path = list(path)
+                    new_path.append(neighbor)
+                    queue.enqueue(new_path)
 
     def getAllSocialPaths(self, userID):
         """
@@ -62,6 +102,9 @@ class SocialGraph:
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
         return visited
+
+    
+        
 
 
 if __name__ == '__main__':
